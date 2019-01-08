7822ae4c25b6fc22164aeeb35c73fc0e0935d31b
Limmy <limych24@gmail.com>
2019-01-09 01:10:54 +0200

    0.03b: Remove downloading background service

--- src/main/java/net/lim/services/BackgroundService.java
+++ src/main/java/net/lim/services/BackgroundService.java
@@ -5,10 +5,7 @@ import org.json.simple.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.net.URL;
 
 @Path("/images")
 public class BackgroundService {
@@ -17,19 +14,5 @@ public class BackgroundService {
     public Response getCurrentImageName() {
         JSONObject current = JsonResponseUtil.getCurrentBackground();
         return Response.ok(current.toJSONString()).build();
-    }
-
-    @GET
-    @Path("/get/{name}")
-    public Response getBackground(@PathParam("name") String backgroundName) {
-        URL resource = this.getClass().getClassLoader().getResource("backgrounds/" + backgroundName);
-        if (resource == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        } else {
-            String fileName = resource.getFile();
-            File f = new File(fileName);
-            System.out.println(f.getPath());
-            return Response.ok(f).header("Content-Disposition", "attachment; filename=\"" + f.getName() + "\"").build();
-        }
     }
 }
