037d2b175bed2ae823c629f0552db3f63bff510b
Limmy <limych24@gmail.com>
2019-03-14 00:02:40 +0200

    0.04b: Issue token after successfull login
    * Send token in json

--- src/main/java/net/lim/services/LoginService.java
+++ src/main/java/net/lim/services/LoginService.java
@@ -1,23 +1,35 @@
 package net.lim.services;
 
 import net.lim.LServer;
+import net.lim.token.Token;
+import net.lim.token.TokenUtils;
+import org.json.simple.JSONObject;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/login")
 public class LoginService {
     @POST
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response login(@FormParam("userName") String userName, @FormParam("pass") String password) {
+        JSONObject entityJSON = new JSONObject();
         if (LServer.connection == null) {
-            return Response.status(500).entity("Server not ready").build();
+            entityJSON.put("message", "Server not ready");
+            return Response.status(500).entity(entityJSON.toJSONString()).build();
         }
         if (LServer.connection.login(userName, password)) {
-            return Response.status(200).build();
+            Token token = TokenUtils.issueToken(userName);
+            entityJSON.put("message", "OK");
+            entityJSON.put("tokenHash", token.getTokenValue());
+            return Response.status(200).entity(entityJSON.toJSONString()).build();
         } else {
-            return Response.status(401).entity("Login failed").build();
+            entityJSON.put("message", "Login failed");
+            return Response.status(401).entity(entityJSON.toJSONString()).build();
         }
     }
 }
