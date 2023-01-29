88272ebb4d5708e6b91b3d4698767ba375313c57
Lim <limych24@gmail.com>
2023-01-29 18:28:56 +0200

    0.05a: Make LServer to pass startup command

--- src/main/java/net/lim/services/ServerListService.java
+++ src/main/java/net/lim/services/ServerListService.java
@@ -4,10 +4,14 @@ package net.lim.services;
 import net.lim.util.JsonResponseUtil;
 import org.json.simple.JSONObject;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import net.lim.util.ConfigReader;
+
+import java.util.Optional;
 
 
 @Path("/servers")
@@ -20,6 +24,23 @@ public class ServerListService {
             return Response.ok(serverInfoJSON.toJSONString(), MediaType.APPLICATION_JSON).build();
         } else {
             return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/startupCommand")
+    @SuppressWarnings("unchecked")
+    public Response getCommandForServer(@QueryParam("serverName") String serverName) {
+        Optional<String> optionalCommandForServer = Optional.ofNullable(
+                ConfigReader.getProperties().getProperty(serverName + ".launchCommand"));
+
+        if (optionalCommandForServer.isPresent()) {
+            JSONObject serverLaunchCommand = new JSONObject();
+            serverLaunchCommand.put(serverName, optionalCommandForServer.get());
+            return Response.ok(serverLaunchCommand.toJSONString(), MediaType.APPLICATION_JSON).build();
+        } else {
+            return Response.status(
+                    Response.Status.BAD_REQUEST.getStatusCode(), "Server " + serverName + " not found").build();
         }
     }
 }
