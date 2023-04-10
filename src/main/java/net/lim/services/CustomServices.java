609fbcf381156e1ccf8a10b39530a67501131e4a
Lim <limych24@gmail.com>
2023-04-10 23:25:54 +0300

    0.05c: Update jersey

--- src/main/java/net/lim/services/CustomServices.java
+++ src/main/java/net/lim/services/CustomServices.java
@@ -6,7 +6,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import net.lim.util.ConfigReader;
 import net.lim.util.VersionUtil;
 
 
