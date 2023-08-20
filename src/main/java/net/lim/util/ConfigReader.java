bf74fc7a6375fdf13c1e621415de93e382afcb71
Lim <limych24@gmail.com>
2023-08-20 22:57:54 +0300

    0.05m: Allow specify ftp creds in separate file

--- src/main/java/net/lim/util/ConfigReader.java
+++ src/main/java/net/lim/util/ConfigReader.java
@@ -3,14 +3,16 @@ package net.lim.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
 
 public class ConfigReader {
 
     private static final String CONFIG_FILE_NAME = "configuration.ini";
-    private static final Properties properties = loadProperties();
+    private static final Properties properties = loadDefaultProperties();
 
+    public static final String EXTERNAL_CONFIG_DIRECTORY = properties.getProperty("ftp.credentials", "/etc/secrets/");
     private static final String minVersionSupported = properties.getProperty("client.minVersion", "0.01a");
 
     public static String getMinVersionSupported() {
@@ -28,17 +30,46 @@ public class ConfigReader {
             throw new RuntimeException(e);
         }
     }
-
+    /**
+     * @deprecated use loadDefaultProperties() or loadProperties(String path) instead
+     * @return Properties from default config file name (resources/configuration/client.config)
+     */
+    @Deprecated
     public static Properties loadProperties() {
-        File configFile = checkConfigExists();
-        Properties properties;
-        try (FileInputStream fileInputStream = new FileInputStream(configFile)){
-            properties = new Properties();
-            properties.load(fileInputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        return loadDefaultProperties();
+    }
+
+    public static Properties loadProperties(String path) {
+        Properties props;
+        try (InputStream resourceConfigStream = new FileInputStream(path)) {
+            verifyStream(resourceConfigStream);
+            props = new Properties();
+            props.load(resourceConfigStream);
+        } catch (Exception e) {
+            throw new RuntimeException("Exception happen when read resource file "
+                    + path + ": " + e.getMessage(), e);
         }
-        return properties;
+        return props;
+    }
+
+    private static void verifyStream(InputStream resourceConfigStream) throws IOException {
+        if (resourceConfigStream == null || resourceConfigStream.available() <= 0) {
+            throw new RuntimeException("No config file found. Please check " + CONFIG_FILE_NAME + " exists.");
+        }
+    }
+
+    public static Properties loadDefaultProperties() {
+        File configFile = checkConfigExists();
+        Properties props;
+        try (InputStream resourceConfigStream = new FileInputStream(configFile)) {
+            verifyStream(resourceConfigStream);
+            props = new Properties();
+            props.load(resourceConfigStream);
+        } catch (Exception e) {
+            throw new RuntimeException("Exception happen when read resource file "
+                    + CONFIG_FILE_NAME + ": " + e.getMessage(), e);
+        }
+        return props;
     }
 
     public static Properties getProperties() {
