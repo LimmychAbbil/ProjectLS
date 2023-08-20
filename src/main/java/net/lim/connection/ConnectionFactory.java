7c4cf3ca3206a32b6d8b492a21ea39dcaa1da42a
Lim <limych24@gmail.com>
2023-08-20 22:57:54 +0300

    0.05m: Allow specify ftp creds in separate file

--- src/main/java/net/lim/connection/ConnectionFactory.java
+++ src/main/java/net/lim/connection/ConnectionFactory.java
@@ -5,16 +5,18 @@ import net.lim.util.ConfigReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConnectionFactory {
 
-    private Properties connectionProperties;
+    private final Properties connectionProperties;
     private static final Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
 
 
     public ConnectionFactory() {
-        connectionProperties = ConfigReader.loadProperties();
+        connectionProperties = ConfigReader.loadDefaultProperties();
     }
 
     public Connection createConnection() {
@@ -47,12 +49,30 @@ public class ConnectionFactory {
     }
 
     public FTPFileGetter createFTPGetter() {
-        boolean isSameHostUsed = "true".equals(connectionProperties.getProperty("ftp.isSameServer"));
-        String ftpHost = connectionProperties.getProperty("ftp.host");
+        Properties externalFTPConfig = null;
+        try {
+            externalFTPConfig = ConfigReader.loadProperties(
+                    ConfigReader.EXTERNAL_CONFIG_DIRECTORY + "ftp.properties");
+        } catch (Exception e) {
+            logger.info("Exception appeared when read external file " + ConfigReader.EXTERNAL_CONFIG_DIRECTORY + "." +
+                    " Would use default config for FTP Connection", e  );
+        }
+        if (externalFTPConfig != null && !externalFTPConfig.isEmpty()) {
+            //load from specific properties
+            return createFTPGetterFromProperties(externalFTPConfig);
+        } else {
+            //load from default properties
+            return createFTPGetterFromProperties(connectionProperties);
+        }
+    }
 
-        int ftpPort = Integer.parseInt(connectionProperties.getProperty("ftp.port"));
-        String ftpUser = connectionProperties.getProperty("ftp.username");
-        boolean usePassive = "true".equals(connectionProperties.getProperty("ftp.usePassive"));
+    private FTPFileGetter createFTPGetterFromProperties(Properties properties) {
+        boolean isSameHostUsed = "true".equals(properties.getProperty("ftp.isSameServer"));
+        String ftpHost = properties.getProperty("ftp.host");
+
+        int ftpPort = Integer.parseInt(properties.getProperty("ftp.port"));
+        String ftpUser = properties.getProperty("ftp.username");
+        boolean usePassive = "true".equals(properties.getProperty("ftp.usePassive"));
         return new FTPFileGetter(ftpHost, ftpPort, ftpUser, isSameHostUsed, usePassive);
     }
 
